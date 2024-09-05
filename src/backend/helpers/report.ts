70cd3bf8acd3dbe195c2c2c0da58f988671d823f
Blockbreaker (aka drippypop) <146481814+TheBlockbreaker@users.noreply.github.com>
2024-09-05 09:19:10 -0400

    Update report.ts

--- src/backend/helpers/report.ts
+++ src/backend/helpers/report.ts
@@ -1,4 +1,4 @@
-import { ScrapeMedia } from "@movie-web/providers";
+import { ScrapeMedia } from "chesko-movie-web";
 import { nanoid } from "nanoid";
 import { ofetch } from "ofetch";
 import { useCallback } from "react";
